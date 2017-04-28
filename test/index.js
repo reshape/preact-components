800fff0b0ac8194fd7851de13025a112e252efbb
Jeff Escalante <escalantejeff@gmail.com>
2017-04-28 17:09:18 -0400

    it works

--- test/index.js
+++ test/index.js
@@ -1,6 +1,18 @@
-const reshape-preact-ssr = require('..')
+const {h} = require('preact')
+const reshape = require('reshape')
+const ssr = require('..')
 const test = require('ava')
 
 test('basic', (t) => {
-  t.is(reshape-preact-ssr, 'test')
+  const MyComponent = ({ foo }) => {
+    return h('p', {}, `the value of foo is "${foo}"`)
+  }
+
+  const html = "<my-component foo='bar' />"
+
+  return reshape({ plugins: [ssr({ 'my-component': MyComponent })] })
+    .process(html)
+    .then((res) => {
+      t.is(res.output(), '<p>the value of foo is "bar"</p>')
+    })
 })
