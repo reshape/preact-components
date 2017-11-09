9dc6577beed4ee559ece8e660170ef69fadbdacc
Jeff Escalante <escalantejeff@gmail.com>
2017-11-09 12:04:21 -0500

    prettier format

--- src/reshape-ast-to-vdom.js
+++ src/reshape-ast-to-vdom.js
@@ -1,20 +1,22 @@
-import {h} from 'preact'
+import { h } from 'preact'
 
-export default function toVnode (components, node, originalHtml) {
+export default function toVnode(components, node, originalHtml) {
   // get element name or component name if registered
   const name = components[node.name] || node.name
   // convert props to strings
   const props = {}
   for (let k in node.attrs) {
-    props[k] = node.attrs[k].map((n) => n.content).join('')
+    props[k] = node.attrs[k].map(n => n.content).join('')
   }
   // if there is a compressed original source, add it as _state prop
-  if (originalHtml) { props._state = originalHtml }
+  if (originalHtml) {
+    props._state = originalHtml
+  }
   // content is either a string, a subtree, or there isn't any
   if (typeof node.content === 'string') {
     return h(name, props, node.content)
   } else if (Array.isArray(node.content)) {
-    const subtree = node.content.map((n) => {
+    const subtree = node.content.map(n => {
       if (n.type === 'tag') return toVnode(components, n)
       if (n.type === 'text') return n.content
     })
--- test/index.js
+++ test/index.js
@@ -1,15 +1,15 @@
 const preact = require('preact')
 const h = preact.h
-const {render} = require('preact-render-to-string')
+const { render } = require('preact-render-to-string')
 const reshape = require('reshape')
 const components = require('..')
 const test = require('ava')
-const {JSDOM} = require('jsdom')
+const { JSDOM } = require('jsdom')
 const fs = require('fs')
 const path = require('path')
 const webpack = require('webpack')
 
-test('basic', (t) => {
+test('basic', t => {
   const MyComponent = ({ foo }) => {
     return h('p', {}, `the value of foo is "${foo}"`)
   }
@@ -18,12 +18,12 @@ test('basic', (t) => {
 
   return reshape({ plugins: [components({ 'my-component': MyComponent })] })
     .process(html)
-    .then((res) => {
+    .then(res => {
       t.is(res.output(), '<p>the value of foo is "bar"</p>')
     })
 })
 
-test('multi element, different props', (t) => {
+test('multi element, different props', t => {
   const MyComponent = ({ foo }) => {
     return h('p', {}, `the value of foo is "${foo}"`)
   }
@@ -32,12 +32,15 @@ test('multi element, different props', (t) => {
 
   return reshape({ plugins: [components({ 'my-component': MyComponent })] })
     .process(html)
-    .then((res) => {
-      t.is(res.output(), '<p>the value of foo is "bar"</p><p>wow</p><p>the value of foo is "wow"</p>')
+    .then(res => {
+      t.is(
+        res.output(),
+        '<p>the value of foo is "bar"</p><p>wow</p><p>the value of foo is "wow"</p>'
+      )
     })
 })
 
-test('renders children', (t) => {
+test('renders children', t => {
   const MyComponent = ({ foo, children }) => {
     return h('div', { class: 'parent' }, 'hello', children)
   }
@@ -46,16 +49,20 @@ test('renders children', (t) => {
     return h('div', { class: 'wow' }, 'hello from c2')
   }
 
-  const html = "<my-component foo='bar'><p>wow</p><div><c2></c2></div></my-component>"
+  const html =
+    "<my-component foo='bar'><p>wow</p><div><c2></c2></div></my-component>"
 
   return reshape({ plugins: [components({ 'my-component': MyComponent, c2 })] })
     .process(html)
-    .then((res) => {
-      t.is(res.output(), '<div class="parent">hello<p>wow</p><div><div class="wow">hello from c2</div></div></div>')
+    .then(res => {
+      t.is(
+        res.output(),
+        '<div class="parent">hello<p>wow</p><div><div class="wow">hello from c2</div></div></div>'
+      )
     })
 })
 
-test('initial state rehydration', (t) => {
+test('initial state rehydration', t => {
   const component = `
     function MyComponent ({ foo, _state }) {
       return preact.h('p', { 'data-state': _state }, 'the value of foo is "' + foo + '"')
@@ -66,12 +73,15 @@ test('initial state rehydration', (t) => {
 
   return reshape({ plugins: [components({ 'my-component': MyComponent })] })
     .process(html)
-    .then((res) => {
-      const dom = new JSDOM(`
+    .then(res => {
+      const dom = new JSDOM(
+        `
         ${res.output()}
         <script>
           // require preact
-          ${fs.readFileSync(path.join(__dirname, '../node_modules/preact/dist/preact.js'))}
+          ${fs.readFileSync(
+            path.join(__dirname, '../node_modules/preact/dist/preact.js')
+          )}
           // require reshapePreact
           ${fs.readFileSync(path.join(__dirname, '../lib/browser.js'))}
           // our custom component
@@ -82,49 +92,76 @@ test('initial state rehydration', (t) => {
             'my-component': MyComponent
           })
         </script>
-      `, { runScripts: 'dangerously' })
+      `,
+        { runScripts: 'dangerously' }
+      )
       t.is(render(dom.window.out), '<p>the value of foo is &quot;bar&quot;</p>')
     })
 })
 
-test('node encode and decode', (t) => {
+test('node encode and decode', t => {
   const data = JSON.stringify({ foo: 'bar' })
   const encoded = components.encode(data)
   const decoded = components.decode(encoded)
   t.is(data, decoded)
 })
 
-test('browser encode and decode', (t) => {
+test('browser encode and decode', t => {
   const data = JSON.stringify({ foo: 'bar' })
-  const dom = new JSDOM(`
+  const dom = new JSDOM(
+    `
     <script>
       // require preact, because its referenced in the browser lib
-      ${fs.readFileSync(path.join(__dirname, '../node_modules/preact/dist/preact.js'))}
+      ${fs.readFileSync(
+        path.join(__dirname, '../node_modules/preact/dist/preact.js')
+      )}
       // require browser lib
       ${fs.readFileSync(path.join(__dirname, '../lib/browser.js'))}
       // implementation
       var encoded = reshapePreact.encode(${data})
       window.decoded = reshapePreact.decode(encoded)
     </script>
-  `, { runScripts: 'dangerously' })
+  `,
+    { runScripts: 'dangerously' }
+  )
   t.is(dom.window.decoded.foo, 'bar')
 })
 
+test.only('custom in-scope data', t => {
+  const MyComponent = ({ foo }) => {
+    return h('p', {}, `the value of foo is "${test}"`)
+  }
+
+  const html = "<my-component foo='bar' />"
+
+  return reshape({ plugins: [components({ 'my-component': MyComponent })] })
+    .process(html)
+    .then(res => {
+      t.is(res.output(), '<p>the value of foo is "bar"</p>')
+    })
+})
+
 // webpack erroring with jsdom for some reason
-test.skip.cb('webpack loads es module version in browser', (t) => {
+test.skip.cb('webpack loads es module version in browser', t => {
   const fixturesPath = path.join(__dirname, 'fixtures')
-  webpack({
-    entry: path.join(fixturesPath, 'entry'),
-    output: { filename: 'bundle.js', path: fixturesPath}
-  }, (err, stats) => {
-    // console.log(fs.readFileSync(path.join(fixturesPath, 'bundle.js'), 'utf8'))
-    const dom = new JSDOM(`
+  webpack(
+    {
+      entry: path.join(fixturesPath, 'entry'),
+      output: { filename: 'bundle.js', path: fixturesPath }
+    },
+    (err, stats) => {
+      // console.log(fs.readFileSync(path.join(fixturesPath, 'bundle.js'), 'utf8'))
+      const dom = new JSDOM(
+        `
       <script>
         ${fs.readFileSync(path.join(fixturesPath, 'bundle.js'), 'utf8')}
       </script>
-    `, { runScripts: 'dangerously' })
-    console.log(dom.window.result)
-    fs.unlinkSync(path.join(fixturesPath, 'bundle.js'))
-    t.end()
-  })
+    `,
+        { runScripts: 'dangerously' }
+      )
+      console.log(dom.window.result)
+      fs.unlinkSync(path.join(fixturesPath, 'bundle.js'))
+      t.end()
+    }
+  )
 })
